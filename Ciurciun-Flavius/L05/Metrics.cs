2ebcf380dae2319bf0f906d23dd1fef7132bf5e5
Ciurciun Flavius <ciurciun.flavius@gmail.com>
2020-11-02 06:49:45 -0800

    L05

---
+++ Ciurciun-Flavius/L05/Metrics.cs
@@ -0,0 +1,14 @@
+using Microsoft.WindowsAzure.Storage.Table;
+namespace Models
+{
+    public class Metrics : TableEntity
+    {
+        public Metrics(string university, string timestamp)
+        {
+            this.PartitionKey = university;
+            this.RowKey = timestamp;
+        }
+        public Metrics(){ }
+        public int Count {get; set;}
+    }
+}
